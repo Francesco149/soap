4f5f7b4649984208c41650d4d6a550dbcc7eb9a2
Franc[e]sco <lolisamurai@tfwno.gf>
2017-07-10 17:01:15 +0200

    add jpeg

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const Pair pairs[] =
 {
 	/* { regex, action } */
 
-	{ "[.](jpg|png|tiff)$", "mupdf-x11 %s" },
+	{ "[.](jpg|jpeg|png|tiff)$", "mupdf-x11 %s" },
 	{ "[.]mp3$",            "mpv --keep-open %s" },
 
 	{
