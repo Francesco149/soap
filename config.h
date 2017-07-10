087850cc9889bc22bce84b9b28e6bfac179cfef9
Franc[e]sco <lolisamurai@tfwno.gf>
2017-07-10 18:20:04 +0200

    use spaces in config.h

--- config.h
+++ config.h
@@ -4,35 +4,35 @@
 
 static const Pair pairs[] =
 {
-	/* { regex, action } */
+    /* { regex, action } */
 
-	{ "[.](jpg|jpeg|png|tiff|pdf|cbz)$", "mupdf-x11 %s" },
-	{ "[.](mp3|ogg|wav|flac|aac)$", "mpv --keep-open %s" },
-	{ "[.](mp4|mkv|avi|mov)$", "mpv --keep-open %s" },
+    { "[.](jpg|jpeg|png|tiff|pdf|cbz)$", "mupdf-x11 %s" },
+    { "[.](mp3|ogg|wav|flac|aac)$", "mpv --keep-open %s" },
+    { "[.](mp4|mkv|avi|mov)$", "mpv --keep-open %s" },
 
-	/*
-		mpv re-downloads the file on every loop so it's best to
-		cache them to /tmp/
-	*/
-#   define TMP_MPV(ext)						\
-	{										\
-		PREHTTP ".*[.]" ext "$",			\
-											\
-		"tmpfile=$(mktemp -u)." ext " && "	\
-		"wget -O $tmpfile %s && "			\
-		"mpv --loop-file $tmpfile && "		\
-		"rm $tmpfile"						\
-	},
+    /*
+        mpv re-downloads the file on every loop so it's best to
+        cache them to /tmp/
+    */
+#   define TMP_MPV(ext)                     \
+    {                                       \
+        PREHTTP ".*[.]" ext "$",            \
+                                            \
+        "tmpfile=$(mktemp -u)." ext " && "  \
+        "wget -O $tmpfile %s && "           \
+        "mpv --loop-file $tmpfile && "      \
+        "rm $tmpfile"                       \
+    },
 
-	TMP_MPV("gif")
-	TMP_MPV("webm")
+    TMP_MPV("gif")
+    TMP_MPV("webm")
 
-	{ "[.]gif$", "mpv --loop-file %s" },
+    { "[.]gif$", "mpv --loop-file %s" },
 
-	{
-		PREHTTP "(youtube.com/watch[?]|youtu[.]be/)",
-		"mpv --keep-open %s"
-	},
+    {
+        PREHTTP "(youtube.com/watch[?]|youtu[.]be/)",
+        "mpv --keep-open %s"
+    },
 
-	{ PREHTTP ".*", "firefox %s" },
+    { PREHTTP ".*", "firefox %s" },
 };
