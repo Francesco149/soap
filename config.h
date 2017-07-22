b2e48c346df15cf797d39c064a86cab9e16f8f57
Franc[e]sco <lolisamurai@tfwno.gf>
2017-07-22 19:23:22 +0200

    add .ps (postscript)

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const Pair pairs[] =
 {
     /* { regex, action } */
 
-    { "[.](jpg|jpeg|png|tiff|pdf|cbz)$", "mupdf-x11 %s" },
+    { "[.](jpg|jpeg|png|tiff|pdf|ps|cbz)$", "mupdf-x11 %s" },
     { "[.](mp3|ogg|wav|flac|aac)$", "mpv --keep-open %s" },
     { "[.](mp4|mkv|avi|mov)$", "mpv --keep-open %s" },
 
